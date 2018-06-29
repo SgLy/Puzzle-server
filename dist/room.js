5a6b22ca51abee9983c53d02848c696f9de599bf
SgLy <775150558@qq.com>
2018-06-29 17:55:32 +0800

    more room api, not tested

--- dist/room.js
+++ dist/room.js
@@ -5,21 +5,60 @@ var rooms = {};
 ;
 var socketIdToName = {};
 function makeRoomClient(socket, username, global) {
+    var currentRoom;
     socketIdToName[socket.id] = username;
-    socket.on('newRoom', function () {
-        rooms[username] = [socket];
-        global.emit('newRoom', username);
+    socket.on('newRoom', function (params) {
+        var split = params.split, pattern = params.pattern;
+        rooms[username] = { master: username, members: [socket], pattern: pattern, split: split };
+        global.emit('newRoom', { username: username, size: 1, pattern: pattern, split: split });
     });
     socket.on('enterRoom', function (master) {
-        rooms[master].forEach(function (socket) {
+        if (currentRoom !== undefined)
+            return;
+        currentRoom = master;
+        rooms[master].members.forEach(function (socket) {
             socket.emit('enterRoom', username);
         });
-        rooms[master].push(socket);
-        socket.emit('roomMember', rooms[master].map(function (s) { return socketIdToName[s.id]; }));
+        rooms[master].members.push(socket);
+        socket.emit('roomMember', rooms[master].members.map(function (s) {
+            return socketIdToName[s.id];
+        }));
         global.emit('changeRoom', {
-            room: master,
-            size: rooms[master].length
+            room: master, size: rooms[master].members.length
         });
+    });
+    socket.on('leaveRoom', function () {
+        if (currentRoom === undefined)
+            return;
+        global.emit('changeRoom', {
+            room: currentRoom, size: rooms[currentRoom].members.length
+        });
+        var i = rooms[currentRoom].members.findIndex(function (s) { return s.id === socket.id; });
+        rooms[currentRoom].members.splice(i, 1);
+        rooms[currentRoom].members.forEach(function (socket) {
+            socket.emit('leaveRoom', username);
+        });
+        currentRoom = undefined;
+    });
+    socket.on('roomList', function () {
+        socket.emit('roomList', Object.values(rooms).map(function (room) { return ({
+            username: room.master,
+            size: room.members.length,
+            pattern: room.pattern,
+            split: room.split
+        }); }));
+    });
+    socket.on('startGame', function () {
+        rooms[username].members.forEach(function (s) { s.emit('startGame'); });
+        delete rooms[username];
+        global.emit('deleteRoom', username);
+    });
+    socket.on('deleteRoom', function () {
+        global.emit('deleteRoom', username);
+        rooms[username].members.forEach(function (s) {
+            s.emit('cancelRoom');
+        });
+        delete rooms[username];
     });
 }
 exports.makeRoomClient = makeRoomClient;
--- src/room.ts
+++ src/room.ts
@@ -1,7 +1,12 @@
 import * as socketio from 'socket.io';
 
 interface RoomList {
-  [key: string]: socketio.Socket[]
+  [key: string]: {
+    master: string,
+    members: socketio.Socket[],
+    pattern: number;
+    split: number
+  }
 };
 const rooms: RoomList = {};
 
@@ -13,20 +18,59 @@ const socketIdToName: SocketIdToName = {};
 export function makeRoomClient(
   socket: socketio.Socket, username: string, global: socketio.Server
 ): void {
+  let currentRoom: string|undefined;
   socketIdToName[socket.id] = username;
-  socket.on('newRoom', () => {
-    rooms[username] = [socket];
-    global.emit('newRoom', username);
+  socket.on('newRoom', (params) => {
+    const { split, pattern } = params;
+    rooms[username] = { master: username, members: [socket], pattern, split };
+    global.emit('newRoom', { username, size: 1, pattern, split });
   });
   socket.on('enterRoom', master => {
-    rooms[master].forEach(socket => {
+    if (currentRoom !== undefined)
+      return;
+    currentRoom = master;
+    rooms[master].members.forEach(socket => {
       socket.emit('enterRoom', username);
     });
-    rooms[master].push(socket);
-    socket.emit('roomMember', rooms[master].map(s => socketIdToName[s.id]));
+    rooms[master].members.push(socket);
+    socket.emit('roomMember', rooms[master].members.map(s =>
+      socketIdToName[s.id]
+    ));
     global.emit('changeRoom', {
-      room: master,
-      size: rooms[master].length
+      room: master, size: rooms[master].members.length
     });
+  });
+  socket.on('leaveRoom', () => {
+    if (currentRoom === undefined)
+      return;
+    global.emit('changeRoom', {
+      room: currentRoom, size: rooms[currentRoom].members.length
+    });
+    let i = rooms[currentRoom].members.findIndex(s => s.id === socket.id);
+    rooms[currentRoom].members.splice(i, 1);
+    rooms[currentRoom].members.forEach(socket => {
+      socket.emit('leaveRoom', username);
+    });
+    currentRoom = undefined;
+  });
+  socket.on('roomList', () => {
+    socket.emit('roomList', Object.values(rooms).map(room => ({
+      username: room.master,
+      size: room.members.length,
+      pattern: room.pattern,
+      split: room.split
+    })));
+  });
+  socket.on('startGame', () => {
+    rooms[username].members.forEach(s => { s.emit('startGame'); });
+    delete rooms[username];
+    global.emit('deleteRoom', username);
+  });
+  socket.on('deleteRoom', () => {
+    global.emit('deleteRoom', username);
+    rooms[username].members.forEach(s => {
+      s.emit('cancelRoom');
+    });
+    delete rooms[username];
   });
 }
